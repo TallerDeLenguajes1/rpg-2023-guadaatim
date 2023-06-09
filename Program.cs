709696c891fff2177035ac989f06005f6df43bf8
guadalupe atim <guadiiatim1309@gmail.com>
2023-06-09 20:08:33 -0300

    lista de personajes

--- Program.cs
+++ Program.cs
@@ -3,10 +3,15 @@
 
 using Personajes;
 
+//un personaje
 Personaje nuevoPersonaje;
-
 FabricaDePersonajes fp = new FabricaDePersonajes();
 nuevoPersonaje = fp.crearPersonaje();
-
 Console.WriteLine(nuevoPersonaje.Nombre);
 
+List<Personaje> listapersonajes = new List<Personaje>();
+
+for (int i = 0; i < 9; i++)
+{
+    listapersonajes.Add(fp.crearPersonaje());
+}
