e31ee6addc36bb074649029d06be08f86706ba41
guadalupe atim <guadiiatim1309@gmail.com>
2023-06-09 20:05:34 -0300

    escribir personaje

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using Personajes;
+
+Personaje nuevoPersonaje;
+
+FabricaDePersonajes fp = new FabricaDePersonajes();
+nuevoPersonaje = fp.crearPersonaje();
+
+Console.WriteLine(nuevoPersonaje.Nombre);
+
