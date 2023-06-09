0d6ced5ffdf1877348c5ddb84f9d068029fea7e9
guadalupe atim <guadiiatim1309@gmail.com>
2023-06-09 20:48:56 -0300

    comentarios

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 //Console.WriteLine("Hello, World!");
 
 using Personajes;
+using System.Text.Json;
 
 //un personaje
 Personaje nuevoPersonaje;
@@ -9,9 +10,12 @@ FabricaDePersonajes fp = new FabricaDePersonajes();
 nuevoPersonaje = fp.crearPersonaje();
 Console.WriteLine(nuevoPersonaje.Nombre);
 
+//lista de personajes
 List<Personaje> listapersonajes = new List<Personaje>();
 
 for (int i = 0; i < 9; i++)
 {
     listapersonajes.Add(fp.crearPersonaje());
 }
+
+//generar 10 personajes y guardarlos
