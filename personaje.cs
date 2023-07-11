e7f191c7b18d6fbbfa5ddf7e2e332f215bb94c38
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-11 20:02:28 -0300

    sacando clase

--- personaje.cs
+++ personaje.cs
@@ -75,24 +75,3 @@ public class FabricaDePersonajes
         return numerorandom;
     }
 }
-
-public class PersonajesJson
-{
-    public void GuardarPersonaje(List<Personaje> listapersonajes, string archivo)
-    {   
-        string personajesjson = JsonSerializer.Serialize(listapersonajes);
-        File.WriteAllText(archivo, personajesjson);
-    }
-
-    public List<Personaje> LeerPersonajes(string archivo)
-    {
-        string jsonstring = File.ReadAllText(archivo);
-        List<Personaje> personajesdesserializados = JsonSerializer.Deserialize<List<Personaje>>(jsonstring);
-        return personajesdesserializados;
-    }
-
-    public bool Existe(string archivo)
-    {
-        return File.Exists(archivo);
-    }
-}
