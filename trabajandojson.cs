00980e06084709ef45d68c4dcecfab4ce2d8f463
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-11 20:00:21 -0300

    guardar y leer personaje

---
+++ trabajandojson.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using Personajes;
+namespace TrabajandoJson;
+
+public class PersonajesJson
+{
+    public void GuardarPersonaje(List<Personaje> listapersonajes, string archivo)
+    {   
+        string personajesjson = JsonSerializer.Serialize(listapersonajes);
+        File.WriteAllText(archivo, personajesjson);
+    }
+
+    public List<Personaje> LeerPersonajes(string archivo)
+    {
+        string jsonstring = File.ReadAllText(archivo);
+        List<Personaje> personajesdesserializados = JsonSerializer.Deserialize<List<Personaje>>(jsonstring);
+        return personajesdesserializados;
+    }
+
+    public bool Existe(string archivo)
+    {
+        return File.Exists(archivo);
+    }
+}
