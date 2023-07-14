f3122de29df21bd2a105026e9a5c5372a54da327
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-14 01:42:38 -0300

    cambio fuente de titulo

--- imagenes.cs
+++ imagenes.cs
@@ -2,38 +2,17 @@ namespace Imagenes;
 
 public class figuras{
 
-    public void Titulo()
-    {
-        Console.WriteLine(@"
-                                                                           ..                                          
-            ..'..                                                              ,;.                                          
-       .',;;clolc:,..                                                  .,.    ':.                                           
-       'lollc;;:llll:.                                               ..:;    'l:.                                           
-       .:oc'.   .;lll' ,:.         .,'                              'cll:.  .coc.                   .    ..                 
-        ,ll,     .:ol, .;.         .c;    ..                       .:lcll. .;lll'         .. ..    ,,.   ,c.                
-        .:oc.    .co:. .'.    .'.. 'l;..','.               ..      'l:':o;.;oc:l;.   .'.  ,c:cl,  .c,    ,l:.               
-        .;ol;. .'coc.  'c'  .:c;...:occc;.       ',.   .. .:.      ,o;..c:'co;'cc. .:lll;.;o::lc..;l.    'll'      ..       
-         .cll::cl:'.   'l, .c:.   .;ll;.        'c:',;';'.,c.      ,l,  ,c:l:.,ll''c:,:o;.;ll:,..,cc,..  .:o:. ..','.       
-          ;llclll:'.   .l;.:l,.    'llc'.      .;cc::cll;,c;.     .:l.  .:ol' 'lc.'l;.;o:.,llc,..,cc;;;'  ,llc:;'.          
-          ,ll,.':clc;. .:,.,cc::,..,:.'cc.     ':.'c:'':c:c:.    .;o:.   'c:. .lc..;lcll,.,;.,c. .c,     .:oc;'.            
-          'll'   ..;cc,.,.  ';..  .:'  'lc.    ..  ..   ...'.     ;c.     ..   ,c'  ....  ..  ;, .:.   .,::,.               
-          .cl,      .,:;.         ..    'c,                       ,c.          .c;            .'..;.  'cc'                  
-           ,l:.       .,:,               ,:.                      ,;            ,:.              .'  'c:.                   
-           .::.         .:,                                       ..             ..               .'::,.                    
-            .'           .'.                                                                    .'''..                      
-                                                                                                ..                         
-        ");
-    }
-
     public void Circulo()
     {
         Console.WriteLine(@"
 
-        ▄▄▄  ▪   ▄▄· ▄ •▄      ▄▄▄·  ▐ ▄ ·▄▄▄▄      • ▌ ▄ ·.       ▄▄▄  ▄▄▄▄▄ ▄· ▄▌
-        ▀▄ █·██ ▐█ ▌▪█▌▄▌▪    ▐█ ▀█ •█▌▐███▪ ██     ·██ ▐███▪▪     ▀▄ █·•██  ▐█▪██▌
-        ▐▀▀▄ ▐█·██ ▄▄▐▀▀▄·    ▄█▀▀█ ▐█▐▐▌▐█· ▐█▌    ▐█ ▌▐▌▐█· ▄█▀▄ ▐▀▀▄  ▐█.▪▐█▌▐█▪
-        ▐█•█▌▐█▌▐███▌▐█.█▌    ▐█ ▪▐▌██▐█▌██. ██     ██ ██▌▐█▌▐█▌.▐▌▐█•█▌ ▐█▌· ▐█▀·.
-        .▀  ▀▀▀▀·▀▀▀ ·▀  ▀     ▀  ▀ ▀▀ █▪▀▀▀▀▀•     ▀▀  █▪▀▀▀ ▀█▄▀▪.▀  ▀ ▀▀▀   ▀ • 
+        ██████╗ ██╗ ██████╗██╗  ██╗     █████╗ ███╗   ██╗██████╗     ███╗   ███╗ ██████╗ ██████╗ ████████╗██╗   ██╗
+        ██╔══██╗██║██╔════╝██║ ██╔╝    ██╔══██╗████╗  ██║██╔══██╗    ████╗ ████║██╔═══██╗██╔══██╗╚══██╔══╝╚██╗ ██╔╝
+        ██████╔╝██║██║     █████╔╝     ███████║██╔██╗ ██║██║  ██║    ██╔████╔██║██║   ██║██████╔╝   ██║    ╚████╔╝ 
+        ██╔══██╗██║██║     ██╔═██╗     ██╔══██║██║╚██╗██║██║  ██║    ██║╚██╔╝██║██║   ██║██╔══██╗   ██║     ╚██╔╝  
+        ██║  ██║██║╚██████╗██║  ██╗    ██║  ██║██║ ╚████║██████╔╝    ██║ ╚═╝ ██║╚██████╔╝██║  ██║   ██║      ██║   
+        ╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝     ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝      ╚═╝   
+                                                                                       
                                 .............                  
                             .';:clooddddddddoolc:;,..            
                         .':loddddk0Odooododdddddddddoc,.         
@@ -106,55 +85,32 @@ public class figuras{
         ");
     }
 
-    // public void Perdedor()
-    // {
-    //     Console.WriteLine("");
-    // }
+    public void Perdedor()
+    {
+        Console.WriteLine(@"
+                                                ......                                               
+                                         .,lxkOkkkkkOkkdl;.                                         
+                                      .:xOOdc;'......';ldO0x:.                                      
+                                    .cO0o,.              .,d0Oc.                                    
+                                   ,OKo.                    .dKk,                                   
+                                  ;K0;     .;'       .,,.     :KK;                                  
+                                 ,00,     .xW0'      :XNl      :K0'                                 
+                                 dNl       .;,       .;;.       oNd.                                
+                                .OK;                            ;Xk.                                
+                                .OK,         ..,;;;;,..         ,Kk.                                
+                                .xX:      .;dxddollodxxo;.      :Nx.                                
+                                 :Xk.    ;xx:.        'ckk,    .kX:                                 
+                                 .oXd.  c0l.            .o0:  .xXo.                                 
+                                  .oKk'.::.              .c;.,kXl.                                  
+                                    ;OKd,                 .,d0k;                                    
+                                     .:kKkl,..        ..,lkKk:.                                     
+                                        'cxOOOkddooodxO0Odl'                                        
+                                           ..,:clcccc:,..                                                                                                    
+        ");
+    }
 
     public void GanadorDelJuego()
     {
-        /*Console.WriteLine(@"
-                                c00000000000000OOOO000000000000000000000000O:.             .........
-                                ,k000000OkkkkkkkkkkkkkkkkO00000000000000000o.        .             .
-                      .,llcc:;;,;d000OkkkkkO000000000OOkkxxkO0000000000000x'                        
-                       .lO000000000kxxkO0000000000000OOO00OkxkO0000000000O;                         
-                        .cO000000OxxO0000000000OkkkkkkkkxO000kxk0000000Ox;.       ..    .........   
-                         .cO0000Oxx000000000OxxkkkkkkkkkxO0000OkxO00000Oolllc:,.........'...........
-                          .:O000xx00000000OxxkOkkkkkkOOO00000000kxO000000000000kxolc:;,,'...........
-                           .:k0kdO000000OkxkOkxkO0000000000000000xxO0000000000000000OOxl,...........
-                            .cOkx0000kkxkkOkxxO000000000000000000Oxk0000000000000000ko:'............
-                       ..;cldk0xokkkkkkOkkxkO0000000000OkO00000000kdO00000000000Oxl:,'..............
-               .       .'ck000kddxkkkkkkkO00OO000000000OkxkO0000000xx000000000kl;'.'''..............
-               ..         .:x0kodkkkkkO000Oxx00000000OOOOkdx0000000Oxk0000000d,.''''''.............'
-                        .   .cxO00000OkkOOxdkkkOOO0d:o0000kdO0000000kxO00000k:.''''''..............'
-          ............  .   .oOkkkkkkxddolld0KXXNWWX0KWWWWKkO0000000Oxk000000koc;'''...............'
-  ........''''''........    ,dkkkO0000OkdloxKWWWWWNWWNWWWNOk000000000xxO00000000Odl:,..............'
-..''''''''''''''........    ,k00000000kkkdkOk0XWWWNNXXKK0kk0000000Okxdox000000000000kdc,..........''
-'''''''''''''''.........   ..:k000000000Okkxdlclolcc:;,,,;ldxO00Odc:cccclk000000000kdl:,..........''
-'''''''.....'''.......'. .;dc..cdxxddolcc:;,,;;:::;;,.....',ckOdc:lkOOOOkxxO000Oxoc;..............''
-''.......''''''........,:dxo;..'''''''';;,'.',,:ccccc;'.....':::lk0000000Oxx0dc:;,;,..............'.
-....''''''''''.......,lkOd,',:cc:;,'...'',,...';clccc;......'':x000000000Oxk0d,',,;,..............'.
-''''''''''''''......'o0Ol'..'',:ccc;'....;c'.';cllllc:,''.....:k0000000Okxk000x:.',,.............''.
-'''''''''''''''..',codko......,:clc;'....'c:'.',;::ccccc:;,,.';d00000OxdxO00000Ol'',.............''.
-'...'''''''''',;lxxxkOx;. ....;ccccc:;''..;dd;....'',;:clcc:,,cok00000kxOxoooollc,...............'..
-....''''''',lxkOxxOOxx0o.  ....';:ccccc:;':k0ko:,''....',;;:cdkxx00000xxOd,.'.''''''.............,. 
-'''''''''''o0OxxOxxOxx0o.   ......',;:c:;:d00000kdoolcccodkO0OxkO0000Odd00x:....................''. 
-''''''''''.cO0ddOddxdxxd;..,:cc:,....':cldkkkkkkkkkkkkO000000OO000000x;,:col,.''............'.....  
-''''''''''.,o0kxxxxdxOO0kdx0Oxoc,.   .;oxkkkkOOOO0000000000000000000k:.''.....,'.......    ...      
-''''''''..'.;x00000Odk0000x:..         .'lkkO0000000000000000000000kc''...''.';'                    
-.............;k0000kdO000o.              .lO000000000000000000000Oo;''....''.,;.                    
-..............,dO00kk0Ox:.                .,dO0000000000000000Okko'.'.. ..''';'.                    
-.....'.''     .lxkkkko,.                    .,lxOO0000000OOkkkkkOx:;,....'''','................     
-......','.....dOkO00Odc'..................,odxkO000OxxxdxkkkkO0OdxO00Okxol:;,''''''''''''.......... 
-......''.....cKNKkk0kxOOl,''''''''.''..':xXWWWWWWNX0kxkkxkkkkkkddKKOXWNWWNXOd:''''''''''............
-,,,,,'......'xWWWNKOxdkOk:.'...'.......'xNWWWWWWN0O0kk000OOOkOOkKWXOKKKWWWWWWKl''''''''.............
-;;;;,''''''.;0WWWWWWNKKKx,.'''''''''..,dNWWWWWWX0KN0kO00000000kONWN0k0NWWWWWWW0:.'''''..............
-;;;,.'''''..oNWNWWWWWWWXl............'dNWWNXNWKOXNKkk00000000OkKWWWOdKWWWWWWWWNd''''''..............
-;;,'''''''.,xWWWWWWWWWWk,...........'dNWWN00NK0XWKxkO00000000kkXWWW0ONWWWWWWWWWO;.'''...............
-;;'.''''''.,OWWWWWWWWWKc'..........,xNWNWXOK00NWNOok000000000kONWWN0KWWWWWWWWWWK:.'''...............
-;,''''''''.:KWWWWWWWWWO;...........dNWWWWKOk0NWWXkdk000000000x0WNWK0NWWWWWWWWWWXl.'''...............
-        ")*/
-
         Console.WriteLine(@"
                         'x0000000OOOkkkkOOO0000000000000000k,             .....
                   .''.....o000OkkkkkkkkkkkkkkkkO00000000000O:                   
