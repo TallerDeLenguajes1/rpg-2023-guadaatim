e41f893ece060c2038be371599f14c70de9d0076
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-27 21:11:28 -0300

    correccion

--- rickandmorty.cs
+++ rickandmorty.cs
@@ -1,44 +1,84 @@
+using System.Text.Json.Serialization;
 namespace RickandMorty;
 
-// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-public class Info
-{
-    public int count { get; set; }
-    public int pages { get; set; }
-    public string next { get; set; }
-    public object prev { get; set; }
-}
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+    public class Info
+    {
+        [JsonPropertyName("count")]
+        public int count { get; set; }
 
-public class Location
-{
-    public string name { get; set; }
-    public string url { get; set; }
-}
+        [JsonPropertyName("pages")]
+        public int pages { get; set; }
 
-public class Origin
-{
-    public string name { get; set; }
-    public string url { get; set; }
-}
+        [JsonPropertyName("next")]
+        public string next { get; set; }
 
-public class Result
-{
-    public int id { get; set; }
-    public string name { get; set; }
-    public string status { get; set; }
-    public string species { get; set; }
-    public string type { get; set; }
-    public string gender { get; set; }
-    public Origin origin { get; set; }
-    public Location location { get; set; }
-    public string image { get; set; }
-    public List<string> episode { get; set; }
-    public string url { get; set; }
-    public DateTime created { get; set; }
-}
+        [JsonPropertyName("prev")]
+        public object prev { get; set; }
+    }
 
-public class RickAndMortyApi
-{
-    public Info info { get; set; }
-    public List<Result> results { get; set; }
-}
+    public class Location
+    {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class Origin
+    {
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+
+        [JsonPropertyName("status")]
+        public string status { get; set; }
+
+        [JsonPropertyName("species")]
+        public string species { get; set; }
+
+        [JsonPropertyName("type")]
+        public string type { get; set; }
+
+        [JsonPropertyName("gender")]
+        public string gender { get; set; }
+
+        [JsonPropertyName("origin")]
+        public Origin origin { get; set; }
+
+        [JsonPropertyName("location")]
+        public Location location { get; set; }
+
+        [JsonPropertyName("image")]
+        public string image { get; set; }
+
+        [JsonPropertyName("episode")]
+        public List<string> episode { get; set; }
+
+        [JsonPropertyName("url")]
+        public string url { get; set; }
+
+        [JsonPropertyName("created")]
+        public DateTime created { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonPropertyName("info")]
+        public Info info { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<Result> results { get; set; }
+    }
