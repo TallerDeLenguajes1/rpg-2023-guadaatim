03b7434de0b809adc35061fa872d4a9a15818a9d
Guadalupe Atim <guadiiatim1309@gmail.com>
2023-07-27 21:15:49 -0300

    correcciones con ?

--- rickandmorty.cs
+++ rickandmorty.cs
@@ -11,28 +11,28 @@ namespace RickandMorty;
         public int pages { get; set; }
 
         [JsonPropertyName("next")]
-        public string next { get; set; }
+        public string? next { get; set; }
 
         [JsonPropertyName("prev")]
-        public object prev { get; set; }
+        public object? prev { get; set; }
     }
 
     public class Location
     {
         [JsonPropertyName("name")]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [JsonPropertyName("url")]
-        public string url { get; set; }
+        public string? url { get; set; }
     }
 
     public class Origin
     {
         [JsonPropertyName("name")]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [JsonPropertyName("url")]
-        public string url { get; set; }
+        public string? url { get; set; }
     }
 
     public class Result
@@ -41,44 +41,44 @@ namespace RickandMorty;
         public int id { get; set; }
 
         [JsonPropertyName("name")]
-        public string name { get; set; }
+        public string? name { get; set; }
 
         [JsonPropertyName("status")]
-        public string status { get; set; }
+        public string? status { get; set; }
 
         [JsonPropertyName("species")]
-        public string species { get; set; }
+        public string? species { get; set; }
 
         [JsonPropertyName("type")]
-        public string type { get; set; }
+        public string? type { get; set; }
 
         [JsonPropertyName("gender")]
-        public string gender { get; set; }
+        public string? gender { get; set; }
 
         [JsonPropertyName("origin")]
-        public Origin origin { get; set; }
+        public Origin? origin { get; set; }
 
         [JsonPropertyName("location")]
-        public Location location { get; set; }
+        public Location? location { get; set; }
 
         [JsonPropertyName("image")]
-        public string image { get; set; }
+        public string? image { get; set; }
 
         [JsonPropertyName("episode")]
-        public List<string> episode { get; set; }
+        public List<string>? episode { get; set; }
 
         [JsonPropertyName("url")]
-        public string url { get; set; }
+        public string? url { get; set; }
 
         [JsonPropertyName("created")]
         public DateTime created { get; set; }
     }
 
-    public class Root
+    public class RickAndMortyApi
     {
         [JsonPropertyName("info")]
-        public Info info { get; set; }
+        public Info? info { get; set; }
 
         [JsonPropertyName("results")]
-        public List<Result> results { get; set; }
+        public List<Result>? results { get; set; }
     }
